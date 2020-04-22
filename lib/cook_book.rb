dffb60cf9fe8e2b65b453983ee9493e019a5481f
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-22 09:30:35 -0600

    Pass test_it_exists

---
+++ lib/cook_book.rb
@@ -0,0 +1,2 @@
+class CookBook
+end
---
+++ test/cookbook_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/cook_book'
+
+
+class CookBookTest < Minitest::Test
+  def setup
+    @cookbook = CookBook.new
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+  end
+
+  def test_it_exists
+    assert_instance_of CookBook, @cookbook
+  end
+end
