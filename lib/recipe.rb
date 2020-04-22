e0ea4614266dfae35a9bad0948cca3a4dfec3b77
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-22 09:27:05 -0600

    Pass test_it_can_add_ingredients

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -4,4 +4,12 @@ class Recipe
     @name = name
     @ingredients_required = {}
   end
+
+  def add_ingredient(ingredient, quantity)
+    if @ingredients_required.keys.include?(ingredient)
+      @ingredients_required[ingredient] += quantity
+    else
+      @ingredients_required[ingredient] = quantity
+    end
+  end
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -20,4 +20,13 @@ class RecipeTest < Minitest::Test
     assert_equal "Mac and Cheese", @recipe1.name
     assert_equal ({}), @recipe1.ingredients_required
   end
+
+  def test_it_can_add_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    expected = { @ingredient1 => 6, @ingredient2 => 8 }
+    assert_equal expected, @recipe1.ingredients_required
+  end
 end
