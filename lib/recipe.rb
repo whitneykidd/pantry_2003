c164ecaf74926eb7ff5445b1d350a16d11c5027b
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-22 09:22:31 -0600

    Pass test_it_has_attributes

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe
+  attr_reader :name, :ingredients_required
   def initialize(name)
     @name = name
+    @ingredients_required = {}
   end
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -15,4 +15,9 @@ class RecipeTest < Minitest::Test
   def test_it_exists
     assert_instance_of Recipe, @recipe1
   end
+
+  def test_it_has_attributes
+    assert_equal "Mac and Cheese", @recipe1.name
+    assert_equal ({}), @recipe1.ingredients_required
+  end
 end
