dc930d8561aa3294e29073cee5cd1b0e011acd39
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-22 09:05:41 -0600

    Pass test_it_has_attributes

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient
+  attr_reader :name, :unit, :calories
   def initialize(info)
     @name = info[:name]
     @unit = info[:unit]
--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -11,4 +11,10 @@ class IngredientTest < Minitest::Test
   def test_it_exists
     assert_instance_of Ingredient, @ingredient1
   end
+
+  def test_it_has_attributes
+    assert_equal "Cheese", @ingredient1.name
+    assert_equal "oz", @ingredient1.unit
+    assert_equal 50, @ingredient1.calories
+  end
 end
