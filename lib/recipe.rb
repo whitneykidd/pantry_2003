52c11dec127b4852f4b290d386b70075559be06d
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-22 09:20:48 -0600

    Pass test_it_exists

---
+++ lib/recipe.rb
@@ -0,0 +1,5 @@
+class Recipe
+  def initialize(name)
+    @name = name
+  end
+end
---
+++ test/recipe_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+
+
+class RecipeTest < Minitest::Test
+  def setup
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, @recipe1
+  end
+end
