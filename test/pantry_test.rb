bca7d690727a90b16d6bde58d8671a2e6d2b5253
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-22 09:08:48 -0600

    Pass test_it_exists

---
+++ test/pantry_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+
+class IngredientTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+end
